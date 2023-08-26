3c5fb4bfac900996535a6d9c7e0081dbc64cccb6
LeeTown <140289459+leetown3113@users.noreply.github.com>
2023-08-26 08:56:54 +0800

    更新 Surge_Networkinfo.js

--- Surge_Networkinfo.js
+++ Surge_Networkinfo.js
@@ -209,7 +209,7 @@ function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
         `运营商：${info.isp}\n` +
         `所在地：${info.country} - ${info.city}`,
       icon: getSSID() ? 'wifi.circle' : 'antenna.radiowaves.left.and.right.circle',
-      'icon-color': getSSID() ? '#285ff4' : '#285ff4',
+      'icon-color': getSSID() ? '#0061fe' : '#0061fe',
     });
   }).catch(error => {
     // 网络切换
@@ -233,7 +233,7 @@ function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
         title: '发生错误',
         content: '无法获取当前网络信息\n请检查网络状态后重试',
         icon: 'wifi.exclamationmark',
-        'icon-color': '#285ff4',
+        'icon-color': '#0061fe',
       });
     }
   });
