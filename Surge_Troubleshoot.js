9c054b0efcdfbec5f1369b7c8c4d3866bc1cf03b
LeeTown <140289459+leetown3113@users.noreply.github.com>
2024-01-25 12:15:40 +0800

    更新 Surge_Troubleshoot.js

--- Surge_Troubleshoot.js
+++ Surge_Troubleshoot.js
@@ -349,9 +349,9 @@ if (typeof $argument !== "undefined" && $argument !== "") {
         });
       result = {
         title: ptitle + ": " + ALL_NUM,
-        content: `MitM${mitm ? "☑" : "☒"}${hostnameNUM}  Script${
-          scripting ? "☑" : "☒"
-        }${ScriptNUM}  Re${rewrite ? "☑" : "☒"}${RewriteNUM}`,
+        content: `MitM${mitm ? "" : ""}${hostnameNUM}  Script${
+          scripting ? "" : ""
+        }${ScriptNUM}  Re${rewrite ? "" : ""}${RewriteNUM}`,
         icon: icons,
         "icon-color": icolor,
       };
